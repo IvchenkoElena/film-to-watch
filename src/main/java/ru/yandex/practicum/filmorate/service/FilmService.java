d4ed6baadf767b03ab06f0efa45ebeb10f36dece
IvchenkoEV <pesok72@mail.ru>
2024-07-02 10:39:49 +0500

    Все методы вынесены в сервисный слой

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -0,0 +1,89 @@
+package ru.yandex.practicum.filmorate.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class FilmService {
+    private final Map<Integer, Film> films = new HashMap<>();
+
+    public Collection<Film> findAllFilms() {
+        return films.values();
+    }
+
+    public Film createFilm(@RequestBody Film newFilm) {
+        // проверяем выполнение необходимых условий
+        if (newFilm.getName() == null || newFilm.getName().isBlank()) {
+            throw new ValidationException("Название не может быть пустым");
+        }
+        if (newFilm.getDescription().length() > 200) {
+            throw new ValidationException("Максимальная длина описания — 200 символов");
+        }
+        if (newFilm.getReleaseDate().isBefore(LocalDate.parse("1895-12-28"))) {
+            throw new ValidationException("Дата релиза — не раньше 28 декабря 1895 года");
+        }
+        if (newFilm.getDuration() <= 0) {
+            throw new ValidationException("Продолжительность фильма должна быть положительным числом");
+        }
+        // формируем дополнительные данные
+        newFilm.setId(getNextId());
+        // сохраняем новый фильм в памяти приложения
+        films.put(newFilm.getId(), newFilm);
+        return newFilm;
+    }
+
+    // вспомогательный метод для генерации идентификатора нового фильма
+    private Integer getNextId() {
+        int currentMaxId = films.keySet()
+                .stream()
+                .mapToInt(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+    public Film updateFilm(@RequestBody Film newFilm) {
+        // проверяем необходимые условия
+        if (newFilm.getId() == null) {
+            throw new ValidationException("Id должен быть указан");
+        }
+        if (films.containsKey(newFilm.getId())) {
+            Film oldFilm = films.get(newFilm.getId());
+            if (newFilm.getName() == null || newFilm.getName().isBlank()) {
+                throw new ValidationException("Название не может быть пустым");
+            }
+            if (newFilm.getDescription().length() > 200) {
+                throw new ValidationException("Максимальная длина описания — 200 символов");
+            }
+            if (newFilm.getReleaseDate().isBefore(LocalDate.parse("1895-12-28"))) {
+                throw new ValidationException("Дата релиза — не раньше 28 декабря 1895 года");
+            }
+            if (newFilm.getDuration() <= 0) {
+                throw new ValidationException("Продолжительность фильма должна быть положительным числом");
+            }
+            // если публикация найдена и все условия соблюдены, обновляем её содержимое
+            oldFilm.setName(newFilm.getName());
+
+            if (newFilm.getDescription() != null) {
+                oldFilm.setDescription(newFilm.getDescription());
+            }
+            if (newFilm.getReleaseDate() != null) {
+                oldFilm.setReleaseDate(newFilm.getReleaseDate());
+            }
+            if (newFilm.getDuration() != null) {
+                oldFilm.setDuration(newFilm.getDuration());
+            }
+            return oldFilm;
+        }
+        throw new NotFoundException("Пост с id = " + newFilm.getId() + " не найден");
+    }
+
+}
