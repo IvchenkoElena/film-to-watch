126a531c4a3745132d41fc308323d1242d252369
IvchenkoEV <pesok72@mail.ru>
2024-10-24 18:47:40 +0500

    Изменения в соответствии с ТЗ12, реализация работы с БД, добавлен слой DAL, репозитории.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,20 +1,32 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 
 @Data
 public class Film {
     private Integer id;
+    @NotBlank
     private String name;
+    @Size(max = 200)
     private String description;
+    @NotNull
     private LocalDate releaseDate;
+    @Positive
     private Integer duration;
     private final Set<Integer> likes = new HashSet<>();
+    private Mpa mpa;
+    private List<Genre> genres = new ArrayList<>();
 
     public int getLikesCount() {
         return likes.size();
